f2289051cc85ebbdfbd5404d52586052fcfcdcbc
redarashid <redarashid824@gmail.com>
2024-10-13 16:57:01 +0300

    first commit

--- src/Components/Common/Header.jsx
+++ src/Components/Common/Header.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../../Imgs/logo.png'
 import { Link } from "react-router-dom"
 import {navlink} from '../Data/dummydata'
+import { Menu } from "@mui/icons-material"
 
 const Header = () => {
+
+    const[responsive, setResponsive] = useState(false)
+
   return (
     <div>
+      <header>
       <div className='container dflexsb'>
         <div className='logo'>
             <img src={logo} alt="" />
@@ -15,7 +20,12 @@ const Header = () => {
                 <Link to={links.url} key={i}>{links.text}</Link>
             ))}
         </div>
+
+        <button onClick={()=> setResponsive(!responsive)} className='toggle'>
+            <Menu className='icon'></Menu>
+        </button>
       </div>
+      </header>
     </div>
   )
 }
